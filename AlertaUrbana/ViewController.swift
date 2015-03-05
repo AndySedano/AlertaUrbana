36b15f1192eb24cf51fd01945b627588df78f4e9
Andy Sedano <andysedano@gmail.com>
2015-03-05 13:33:01 -0600

    Initial Commit

---
+++ AlertaUrbana/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AlertaUrbana
+//
+//  Created by AndyE on 3/5/15.
+//  Copyright (c) 2015 Ultron. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
